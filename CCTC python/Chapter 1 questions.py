2ff0bd1eb78cd7486d08362f8a5e1e99cc90907c
Geoff <Geoffrey.V.Herz@gmail.com>
2018-03-15 18:57:29 -0700

    Added question 6 and fixed potential error
    Used str as a variable name - this is a mistake since str() is a standard function, having a variable declared as str will cause problems when trying to use str()

--- CCTC python/Chapter 1 questions.py
+++ CCTC python/Chapter 1 questions.py
@@ -3,14 +3,14 @@
 #Is  Unique:  Implement an algorithm  to  determine  if  a string  has  all unique characters. 
 #What if you can't use additional data structures?
 
-def is_Unique(str):
-	return len(str) == len(set(str))
+def is_Unique(str1):
+	return len(str1) == len(set(str1))
 
 #No additional data structures	
-def is_Unique_alt(str):
-	str.sort()
-	for i in range(len(str)-1):
-		if str[i] != str[i+1]
+def is_Unique_alt(str1):
+	str1.sort()
+	for i in range(len(str1)-1):
+		if str1[i] != str1[i+1]
 			return false
 	return true
 
@@ -29,15 +29,15 @@ def isPerm(str1, str2):
 # You  may assume  that  the  string has sufficient space at  the  end  to hold  the  additional characters,  and  that  you are given  the  "true"
 # length of  the  string.
 
-def URL1(str):
-    listy = ['20%' if char == ' ' else char for char in str]
+def URL1(str1):
+    listy = ['20%' if char == ' ' else char for char in str1]
     return ''.join(listy)
 	
-def URL2(str):
-    return '20%'.join(listy.split())
+def URL2(str1):
+    return '20%'.join(str1.split())
 
-def URL3(str):
-    return str.replace(' ', '20%')
+def URL3(str1):
+    return str1.replace(' ', '20%')
 	
 	
 #////////////////////////////////////////#
@@ -47,11 +47,26 @@ def URL3(str):
 # permutation  is  a rearrangement  of  letters. The palindrome does  not  need to  be  limited to just
 # dictionary words.
 
-def is_Permutation(str):
+def is_Permutation(str1):
     odd = 0
     for char in set(str):
         if str.count(char)%2 != 0:
             odd+=1
     return odd<=1
 
+#////////////////////////////////////////#
+#			Question 6
+# String Compression:  Implement a method to perform basic string compression using  the  counts
+# of repeated characters.  For  example,  the  string  aabcccccaaa  would become  a2blc5a3 .  If  the
+# "compressed" string would not become smaller than  the  original string, your method should return
+# the  original string.  You  can assume the string has only uppercase and lowercase letters  (a  - z).
+
+from itertools import groupby
+
+def compress(str1):
+    s = ''
+    for k, g in groupby(str1):
+        rep = len(list(g))
+        s += k + str(rep)
+    return s
 	

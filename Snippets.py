99b6bf58dbd09169555770d867fc051141fea6e9
Geoff <Geoffrey.V.Herz@gmail.com>
2018-04-13 19:08:12 -0700

    Update Snippets.py

--- Snippets.py
+++ Snippets.py
@@ -8,39 +8,21 @@ This is all snippet code. The file isn't meant to be run as a whole.
 
 tinyList = ['1',2,'3']
 x, y, z = tinyList    #List size must = the number of variables exactly!
-print("One here: ",  x)     #>>> One here:  1
-print("Two here: ", y)      #>>> Two here:  2
-print("Three here: ", z)    #>>> Three here:  3
+
+x #>>> 1
+y #>>> 2
+Z #>>> 3
 
 x, *y, z, q = [1,2,3,4,5,6,7]      #But you can cheat the above rule doing this.
-print("One here: ", x) 
-#>>> One here:  1
-print("All numbers up till the last two: ", y)
-#>>> All numbers up till the last two:  [2, 3, 4, 5]
-print("Six here: ", z)
-#>>> Six here:  6
-print("Seven here: ", q)
-#>>> Seven here:  7
+
+x #>>> 1
+y #>>> [2, 3, 4, 5]
+z #>>> 6
+q #>>> 7
 
 x, *y = [1,2,3,4,5,6,7]
 print("All numbers following 1 here: ", y)
 #>>> All numbers following 1 here:  [2, 3, 4, 5, 6, 7]
-
-#One line variable switiching using quick assignment
-x = 10
-y = 'Ten'
-x, y = y, x
-print(x, y)
-#>>> Ten 10
-
-#Works with tuples too:
-(a, (b, c), d) = [(1, 2), (3, 4), (5, 6)]
-print(a)
-#>>> (1, 2)
-print(b)
-#>>> 3
-print(c, d)
-#>>> 4 (5, 6)
 
 #Also handy when doing partitions. E.G. given 45.60 get 45 and 65 in seperate variables:
 num = 45.65
@@ -49,10 +31,35 @@ print(x) #>>> 45
 print(y) #>>> .
 print(z) #>>> 65
 
+#Works with tuples too:
+(a, (b, c), d) = [(1, 2), (3, 4), (5, 6)]
+
+a #>>> (1, 2)
+b #>>> 3
+print(c, d)  #>>> 4 (5, 6)
+
 #Functions like map or generics work with quick assignment, so long as you're absolutely sure the # of variables the function returns
 x, y, z = (i+1 for i in range(3))	
 x, y, z = map(int, tinyList)
 x, y, z = [int(i) for i in input("Type 3 numbers: ").split()]
+
+#One line variable switiching using quick assignment is also possible with variable unpacking. 
+x = 10
+y = 'Ten'
+x, y = y, x
+
+print(x, y)  #>>> Ten 10
+
+#///////////////////////////////////////////////////////////////////#
+# Getting a function to return multiple variables using above variable unpacking. 
+def function():
+	return 1, 2, 3
+
+x, y, z = function()
+print(x, y, z )     #Variables saved as ints
+print(function())   #Function() returns as a tuple.
+#>>> 1 2 3
+#>>> (1, 2, 3)
 
 #///////////////////////////////////////////////////////////////////#
 #Creating new types in a dynamic manner
@@ -70,18 +77,13 @@ myNewType = NewType()
 print(myNewType.function)
 #>>> I'm NewType's function
 
-
 #///////////////////////////////////////////////////////////////////#
-#Dir is a gem that should be used more often when you want to know exactly what an object you've got can do. 
-
-print(dir("Strings!"))
-#>>> Everything built-in to strings. It's a long list. 
-
-#///////////////////////////////////////////////////////////////////#
-#Shortcuts in if-else syntax, useful for confusing co-workers
+#Shortcuts in if-else syntax
 #[on_true] if [expression] else [on_false]
 #Reads exactly as english basically. It's neat.
 #if you start using nested if-else using this, co-workers will inevitably murder you.
+#Main use is in loop comprehensions. 
+
 n = 10
 print("This should be printed") if n == 10 else print("Ruh roh")
 #>>> This should be printed
@@ -143,6 +145,14 @@ listy = [1,2,3,4,5,6,7,8,10]
  		for y in range(4):
 			add (x,y) to the list
 
+#Another example of using if-else inside a loop comp. 
+myString = "P#leas^e///# remo0ve w^eird **(chara!cters!?) +=-"
+fixedList = [i for i in myString if i.isalpha() or i.isspace()]
+fixedString = ''.join(fixedList)
+print(fixedString)
+#>>> Please remove weird characters
+
+
 #///////////////////////////////////////////////////////////////////#
 #Things you can do with negatives in the index spot
 
@@ -158,7 +168,7 @@ print(type(myList[-1:]))
 
 
 #///////////////////////////////////////////////////////////////////#
-#More things you can do with slicing
+#More things you can do with slicing (the : operator)
 #General rule: [ <Start> : <End> : <Step Size> ]
 #If you don't fill out a <> it falls to it's default:
 #<Start = 0>, <End = Ending Index>, <Step Size = 1>
@@ -166,10 +176,9 @@ print(type(myList[-1:]))
 myList = [1,2,"H","e","y",3,4,5]
 print("Get Hey ", myList[2:5])
 print("Get Hey ", myList[-6:-3])
-#>>> print("Reverse list: ", myList)
 #>>> Get Hey  ['H', 'e', 'y']
 #>>> Get Hey  ['H', 'e', 'y']
-#>>> Reverse list:  [1, 2, 'H', 'e', 'y', 3, 4, 5]
+
 """
 Index -> Value
     0 1
@@ -216,24 +225,12 @@ print(myArray)
 
 
 #///////////////////////////////////////////////////////////////////#
-#Other neat things with the * operator.
+#Other neat things with the * operator. Using it to multiply lists. 
 
 #This works the exact same with lists and sets. 
 myString = "xyz" * 3
 print(myString)
 #>>> xyzxyzxyz
-
-
-#///////////////////////////////////////////////////////////////////#
-# Getting a function to return multiple variables
-def function():
-	return 1, 2, 3
-
-x, y, z = function()
-print(x, y, z )     #Variables saved as ints
-print(function())   #Function() returns as a tuple.
-#>>> 1 2 3
-#>>> (1, 2, 3)
 
 
 #///////////////////////////////////////////////////////////////////#
@@ -299,7 +296,7 @@ print(combinedDict)
 #>>> {'y': 5, 'x': 6, 'z': 3, 'a': 2}
 
 #///////////////////////////////////////////////////////////////////#
-#Flatten a nested list
+#Flatten a nested list using the sum operator. 
 myList = [['one', 'two', 'three', 'four'],[1,2,3,4]]
 sum(myList, [])
 #>>> ['one', 'two', 'three', 'four', 1, 2, 3, 4]
@@ -332,10 +329,8 @@ numInList = len(set(myList))
 print("Amount of unique numbers in myList: ", numInList) 
 
 
-
-
 #///////////////////////////////////////////////////////////////////#
-#You can generating Dicts on the go
+#You can generating Dicts on the go using loop comprehensions. 
 myList = {x: x ** 2 for x in range(5)}
 print(myList)
 #>>> {0: 0, 1: 1, 2: 4, 3: 9, 4: 16}
@@ -346,14 +341,13 @@ print(myList)
 
 
 #///////////////////////////////////////////////////////////////////#
-#Merging a 2D list into a 1D list side by side
+#More time-efficient way of flattening a nested list. 
 import itertools
 
 mylist = [['One', 2], ['Three', 4], ['Five', 6]]
 fixedList = list(itertools.chain.from_iterable(mylist))
 print(fixedList)
 #>>> ['One', 2, 'Three', 4, 'Five', 6]
-
 
 
 #///////////////////////////////////////////////////////////////////#
@@ -395,17 +389,8 @@ print(newList, type(newList[2]))
 
 
 #///////////////////////////////////////////////////////////////////#
-#Using loop comprehensions with if statments
-myString = "P#leas^e///# remo0ve w^eird **(chara!cters!?) +=-"
-fixedList = [i for i in myString if i.isalpha() or i.isspace()]
-fixedString = ''.join(fixedList)
-print(fixedString)
-#>>> Please remove weird characters
-
-
-#///////////////////////////////////////////////////////////////////#
 #Other cool uses putting generators with loop comprehensions, lets you escape intermediate storage
-#Can be useful to speed things up.
+#Can be useful to speed things up since generators use lazy evaluation to shortcut some processing power. 
 # x=(n for n in foo if bar(n))
 # for n in x:
 
@@ -486,7 +471,7 @@ print(max(mySet, key = myList.count))
 
 
 #///////////////////////////////////////////////////////////////////#
-#Print out the most repeating number, using counter shenanigans
+#Print out the most repeating number, using counter shenanigans. Longer but far more thorough. 
 import collections
 
 myList = [1,2,3,4,2,2,2,1,4,4,4,7,3,2,8,6,4,1,2,9]
@@ -555,15 +540,18 @@ myDict = { e[0]:e for e in myList}
 print(myDict)
 #>>> {'KEY': ['KEY', 'B', 7, 7, 7], 1: [1, 'A'], 3: [3, 'C', 'HELLO WORLD!']}
         
+	
 #///////////////////////////////////////////////////////////////////#
 #Python considers almost everything as true if used as a bool except for: None, 0, False, '', (), [], {}
 #Can be useful in some cases, esspecially when paired with And, Or, Not, ect ect
-#In this example, using '' as false specifically for the "or num" part. Since empty string + another empty string is an empty string. 
+#In this example, using '' as false specifically for the "or num" part. Since empty string + another empty string is an empty string.
+#And anything multiplied by 0 is 0, so a string multiplied by 0 is an empty string. 
 
 size = 15
 ['Fizz' * (not num%3) + 'Buzz' * (not num%5) or num for num in range(size)][1:]
 #>> [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14]
 #Can also replace range(size) with range(1, size) if slice isn't prefered. 
+
 
 #///////////////////////////////////////////////////////////////////#
 #Filter( foo, bar) where foo returns true/false and is applied on every element in bar
@@ -583,6 +571,7 @@ listy1 = [2,3,15,5,6,11,1]
 listy2 = [1,10,11,7,12,15]
 findIntersection(listy1, listy2)
 #>>> [1, 11, 15]
+
 
 #///////////////////////////////////////////////////////////////////#
 #Intertools's groupby is interesting. 
